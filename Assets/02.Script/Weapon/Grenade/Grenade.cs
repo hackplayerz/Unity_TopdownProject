2628fde806d2b17c95aa0110f6bc0f687b9789b4
NikuRamen <dev.alter0@gmail.com>
2022-11-04 17:51:26 +0900

    11-04 todo:인벤토리

--- Assets/02.Script/Weapon/Grenade/Grenade.cs
+++ Assets/02.Script/Weapon/Grenade/Grenade.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8947b9bde956a7bff534a07e83c9189b22cf3bd3c4d54c686f33bd102181d48f
-size 1749
+oid sha256:a55b80497a64bf118efc0524dec8920f15c25003234c508015da8ee64dcafcda
+size 1821
