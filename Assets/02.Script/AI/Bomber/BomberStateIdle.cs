f76381ac5c769e382922625a5e2df5df3962de5f
Dev.NikuRamen <dev.alter0@gmail.com>
2023-01-05 14:25:59 +0900

    01-05 레벨 데이터 변경

--- Assets/02.Script/AI/Bomber/BomberStateIdle.cs
+++ Assets/02.Script/AI/Bomber/BomberStateIdle.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b3b17cbca3026787f5f252ffb9a68437baebce3b8379125965399dfd8097df26
-size 964
+oid sha256:5a149e5febf6ddc5e506d72e67db29d8c752cc69d393d322c63d8f5f2f02b1b8
+size 974
